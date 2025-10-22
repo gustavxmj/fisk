ea1c355cd2588cdd0be2c1019341fc285e8d8fc6
Gustav X <sillygorilla42@gmail.com>
2025-10-22 17:50:21 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+scene.setBackgroundColor(9)
+tiles.setCurrentTilemap(tilemap`level1`)
+let fisk = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . c c c c . . . . . . . . 
+    . . . c d d d d c c . . . . . . 
+    . . . c d c c c c c c . . . . . 
+    . . . c c d 4 4 4 4 c c . . . . 
+    c c . c 1 4 4 4 4 4 d 4 c . . . 
+    c 4 c 1 d 4 4 4 4 1 4 4 4 c . . 
+    c 4 c 1 4 4 4 4 4 1 4 4 4 4 c . 
+    f 4 4 1 4 4 4 4 4 1 4 4 4 4 4 f 
+    f 4 f 1 4 4 4 c c 1 4 f 4 4 4 f 
+    f 4 f d 4 4 f 4 4 1 4 4 4 4 4 f 
+    f f f f d 4 f 4 c 1 4 4 4 4 f . 
+    . . c f c 4 f f 4 4 d 4 f f . . 
+    . . c b d c 4 4 4 4 f f . . . . 
+    . . c d d d f f f f . . . . . . 
+    . . . c c c . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(fisk)
+scene.cameraFollowSprite(fisk)
---
+++ tilemap.g.ts
@@ -0,0 +1,43 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000002000003000001000000010000000000000000000000000000000000020000030000010000000000030000000000000000000000000000020000000100000000000000000000000000000300000002000000020000000000000000030000000001000000000000000100000000000000000003000002000000000000000100000000000000000000000300000000000200000000000000000000010000000000000100000000000100000000020000000000000300000000000000000000000000000000000000000000020000000000000000000000000000000000000000010000000200`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.builtin.oceanSand3,sprites.builtin.coral0,myTiles.tile1], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
